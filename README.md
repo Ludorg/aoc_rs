e8d518adf7c213e2c9ad353cfc48ca20048b530a
Ludorg@Cyndor_WSL <ludorg@ludorg.net>
2021-12-14 22:38:03 +0100

    day 1 step 1

---
+++ README.md
@@ -0,0 +1,7 @@
+# Advent of Code 2021 in Rust 🦀 by Ludorg
+
+## Day 1 - Step 1 ⭐
+
+See instructions [here](https://adventofcode.com/2021/day/1)
+
+## Day 1 - Step 2
---
+++ private/day1/src/main.rs
@@ -0,0 +1,46 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    // file
+    let filename = "input.txt";
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    // vector to sore data
+    let mut vec: Vec<i32> = Vec::new();
+
+    // read line into vector
+    for (_index, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+
+        let val: i32 = line.parse().unwrap();
+        vec.push(val);
+
+        //println!("{}: {}", _index + 1, line);
+    }
+
+    // ----------
+    // step1
+
+    // not optimal loop for vector
+    let mut index = 0;
+    let mut count = 0;
+    println!("0: {} (N/A)", vec[0]);
+    while index < vec.len() - 1 {
+        if vec[index] < vec[index + 1] {
+            count += 1;
+            println!("{}: {} (increased)", index + 1, vec[index + 1]);
+        }
+        if vec[index] > vec[index + 1] {
+            println!("{}: {} (decreased)", index + 1, vec[index + 1]);
+        }
+        if vec[index] == vec[index + 1] {
+            println!("{}: {} (no change)", index + 1, vec[index + 1]);
+        }
+        index += 1;
+    }
+    println!("{} ", count);
+
+    // step2
+}
