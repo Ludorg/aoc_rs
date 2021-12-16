655e5b2a2c915fdb2946b809769cf6909835e7b4
Ludorg@Cyndor_WSL <ludorg@ludorg.net>
2021-12-16 23:38:15 +0100

    day 2 step 1

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,4 +1,35 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    // TODO
-    println!("Hello, world!");
+    // file
+    let filename = "input.txt";
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    // pos
+    let mut h_pos: i32 = 0;
+    let mut depth: i32 = 0;
+
+    // step 1
+    for (_index, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+
+        let mut splitted = line.split(' ');
+        let direction = splitted.next().unwrap();
+        let value: i32 = splitted.next().unwrap().parse().unwrap();
+
+        match &direction as &str {
+            "up" => {
+                depth -= value;
+            }
+            "down" => {
+                depth += value;
+            }
+            "forward" => {
+                h_pos += value;
+            }
+            _ => {}
+        }
+    }
+    println!("{}", h_pos * depth);
 }
