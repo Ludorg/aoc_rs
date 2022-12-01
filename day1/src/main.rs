8a34f67dd0dca17161d61875d03e791ef9390648
Ludorg <ludorg@ludorg.net>
2022-12-01 23:36:16 +0000

    day 1 completed

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,38 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "input.txt";
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    // vector to store data (calories carried by elves)
+    let mut elves: Vec<i32> = Vec::new();
+
+    // Current elf
+    let mut current_elf: i32 = 0;
+
+    for (_index, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+        // if read value is empty, store accumulated calories by an elf
+        if line.is_empty() {
+            elves.push(current_elf);
+            current_elf = 0;
+        } else {
+            let val: i32 = line.parse().unwrap();
+            current_elf += val;
+        }
+    }
+
+    // reverse sort of elves vector
+    elves.sort_by(|a, b| b.cmp(a));
+
+    // part 1
+    println!("maximum calories carried is: {}", elves[0]);
+
+    // part 2
+    println!(
+        "the top three Elves carrying the most Calories are carrying: {}",
+        elves[0] + elves[1] + elves[2]
+    );
 }
