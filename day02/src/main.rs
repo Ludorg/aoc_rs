5962d683a95edd6ba035e34c6e2b0dec3aaf5fa0
Ludorg <ludorg@ludorg.net>
2022-12-02 23:20:06 +0000

    day2 part2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -6,15 +6,57 @@ fn main() {
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
-    let mut val: i32 = 0;
+    // part1
+    let mut val1: i32 = 0;
+
+    // part2
+    let mut val2: i32 = 0;
 
     for (_index, line) in reader.lines().enumerate() {
         let line = line.unwrap();
         let chars: Vec<_> = line.chars().collect();
-        println!("Elfo played: {}, I played: {}", chars[0], chars[2]);
-        val += compute(chars[0], chars[2]);
+        println!(
+            "Elfo plays: {}, I need to play : {}",
+            elfo_action(chars[0]),
+            my_action(chars[2])
+        );
+        val1 += compute1(chars[0], chars[2]);
+        println!(
+            "Elfo plays: {}, I need to: {}",
+            elfo_action(chars[0]),
+            my_action_2(chars[2])
+        );
+        val2 += compute2(chars[0], chars[2]);
     }
-    println!("My score is {}", val);
+    println!("My score for part 1 is {}", val1);
+    println!("My score for part 2 is {}", val2);
+}
+
+fn elfo_action(e: char) -> &'static str {
+    match e {
+        'A' => "Rock",
+        'B' => "Paper",
+        'C' => "Scissors",
+        _ => "Nothing (this is an error)",
+    }
+}
+
+fn my_action(e: char) -> &'static str {
+    match e {
+        'X' => "Rock",
+        'Y' => "Paper",
+        'Z' => "Scissors",
+        _ => "Nothing (this is an error)",
+    }
+}
+
+fn my_action_2(e: char) -> &'static str {
+    match e {
+        'X' => "lose",
+        'Y' => "draw",
+        'Z' => "win",
+        _ => "Nothing (this is an error)",
+    }
 }
 
 // A : rock
@@ -26,7 +68,7 @@ fn main() {
 // 0 loss
 // 3 draw
 // 6 win
-pub fn compute(a: char, y: char) -> i32 {
+pub fn compute1(a: char, y: char) -> i32 {
     match a {
         'A' => match y {
             // Elfo plays Rock
@@ -53,16 +95,60 @@ pub fn compute(a: char, y: char) -> i32 {
     }
 }
 
+// A : rock
+// B : paper
+// C : scissors
+// X : Lose
+// Y : Draw
+// Z : Win
+pub fn compute2(a: char, y: char) -> i32 {
+    match a {
+        'A' => match y {
+            // Elfo plays Rock
+            'X' => 3 + 0, // Loss needed, play Scissors (3)
+            'Y' => 1 + 3, // Draw, play Rock (1)
+            'Z' => 2 + 6, // Win, play paper (2)
+            _ => 0,
+        },
+        'B' => match y {
+            // Elfo plays Paper
+            'X' => 1 + 0, // Loss, play Rock (1)
+            'Y' => 2 + 3, // Draw, play Paper (2)
+            'Z' => 3 + 6, // Win, play Scissors (3)
+            _ => 0,
+        },
+        'C' => match y {
+            // Elfo plays Scissor
+            'X' => 2 + 0, // Loss, play Paper (2)
+            'Y' => 3 + 3, // Draw, play Scissors (3)
+            'Z' => 1 + 6, // Win, play Rock (1)
+            _ => 0,
+        },
+        _ => 0,
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::compute;
+    use super::compute1;
     #[test]
     fn example_part1() {
-        assert_eq!(compute('A', 'Y'), 8);
-        assert_eq!(compute('B', 'X'), 1);
-        assert_eq!(compute('C', 'Z'), 6);
-        assert_eq!(compute('D', 'E'), 0);
-        assert_eq!(compute('A', 'E'), 0);
-        assert_eq!(compute('E', 'X'), 0);
+        assert_eq!(compute1('A', 'Y'), 8);
+        assert_eq!(compute1('B', 'X'), 1);
+        assert_eq!(compute1('C', 'Z'), 6);
+        assert_eq!(compute1('D', 'E'), 0);
+        assert_eq!(compute1('A', 'E'), 0);
+        assert_eq!(compute1('E', 'X'), 0);
+    }
+
+    use super::compute2;
+    #[test]
+    fn example_part2() {
+        assert_eq!(compute2('A', 'Y'), 4);
+        assert_eq!(compute2('B', 'X'), 1);
+        assert_eq!(compute2('C', 'Z'), 7);
+        assert_eq!(compute2('D', 'E'), 0);
+        assert_eq!(compute2('A', 'E'), 0);
+        assert_eq!(compute2('E', 'X'), 0);
     }
 }
