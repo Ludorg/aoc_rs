6ca4ad10893cab5a77f93e99b8bade289a77a83d
Ludorg (tha_win) <ludorg@ludorg.net>
2023-12-15 14:20:19 +0100

    day15/part1

---
+++ 2023/day15/src/main.rs
@@ -0,0 +1,61 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn compute_substring(s: &str) -> u32 {
+    let mut current_value = 0;
+    for c in s.chars() {
+        current_value += c as u32;
+        current_value *= 17;
+        current_value %= 256;
+    }
+    current_value
+}
+
+fn compute_line(s: &str) -> u32 {
+    let mut ret = 0;
+    for ss in s.split(',') {
+        ret += compute_substring(ss);
+    }
+    ret
+}
+
+fn compute_file(filename: &str) -> u32 {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    for (_index, line) in reader.lines().enumerate() {
+        let item = &line.unwrap();
+
+        return compute_line(item.as_str());
+    }
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_compute_line() {
+        assert_eq!(
+            compute_line("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"),
+            1320
+        );
+    }
+
+    #[test]
+    fn test_compute_file() {
+        assert_eq!(compute_file("example.txt"), 1320);
+    }
+
+    #[test]
+    fn test_compute_substring() {
+        assert_eq!(compute_substring("rn=1"), 30);
+        assert_eq!(compute_substring("cm-"), 253);
+        assert_eq!(compute_substring("qp=3"), 97);
+    }
+}
+
+fn main() {
+    println!("day15/part1={}", compute_file("2023/day15/example.txt"));
+}
