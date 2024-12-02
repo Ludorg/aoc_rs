f7bbca08c31f7603964d99149da6f23629c02bcf
Ludorg (tha_win) <ludorg@ludorg.net>
2024-12-02 15:19:55 +0100

    2024/day02/p2 ok, but quick and dirty...

--- 2024/day02/src/main.rs
+++ 2024/day02/src/main.rs
@@ -10,13 +10,13 @@ fn is_safe(v: &Vec<i32>) -> bool {
     for i in 1..(v.len()) {
         if is_increasing {
             let diff = v[i] - v[i - 1];
-            println!("+ / {diff}");
+            // println!("+ / {diff}");
             if diff > 3 || diff == 0 || diff < 0 {
                 return false;
             }
         } else {
             let diff = v[i - 1] - v[i];
-            println!("- / {diff}");
+            // println!("- / {diff}");
             if diff > 3 || diff == 0 || diff < 0 {
                 return false;
             }
@@ -25,9 +25,26 @@ fn is_safe(v: &Vec<i32>) -> bool {
     true
 }
 
+fn is_safe_2(v: &Vec<i32>) -> bool {
+    let safe = is_safe(v);
+    if safe {
+        return true;
+    }
+    // remove and element between 0..len and try if safe
+    for i in 0..(v.len()) {
+        let mut v2 = v.clone();
+        v2.remove(i);
+        println!("v2={:?}", v2);
+        if is_safe(&v2) == true {
+            println!("is safe");
+            return true;
+        }        
+    }
+    false
+}
+
 fn main() {
     //let filename = "2024/day02/test.txt";
-
     let filename = "2024/day02/input.txt";
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
@@ -39,14 +56,14 @@ fn main() {
             .split_whitespace()
             .map(|s| s.parse().expect("invalid number"))
             .collect();
-        let is_safe = is_safe(&levels);
-        println!(
-            "i={}/ line={} / levels={:?} / is_safe={}",
-            _index + 1,
-            line,
-            levels,
-            is_safe
-        );
+        let is_safe = is_safe_2(&levels);
+        // println!(
+        //     "i={}/ line={} / levels={:?} / is_safe={}",
+        //     _index + 1,
+        //     line,
+        //     levels,
+        //     is_safe
+        // );
         if is_safe {
             safe_reports += 1
         }
